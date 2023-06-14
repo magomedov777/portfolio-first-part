f540dd26cdb6698ecd1288e992d5d8447c84ce83
Arkadiy <alibullterrier@gmail.com>
2023-06-14 06:22:34 +0300

    rm import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import { Header } from './Header/Header';
-import { Navbar } from './Navbar/Navbar';
 import { Main } from './Main/Main';
 import { Skills } from './Skills/Skills';
 
